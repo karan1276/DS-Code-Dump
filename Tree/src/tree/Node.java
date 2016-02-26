5252c430bf7480f4a07c6e110b11c0eaba3febcd
Karan Sharma <karan1276@gmail.com>
2016-02-26 09:05:35 +0530

    Writing a tree class

---
+++ Tree/src/tree/Node.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tree;
+
+/**
+ *
+ * @author karan
+ */
+public class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    public Node() {
+        this(0);
+    }
+    
+    public Node(int data) {
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+    
+}
